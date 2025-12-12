3b0b8dabd22171c3557f54be0828fe2860191dca
zhz1993 <zhz19930606@hotmail.com>
2025-12-12 14:12:33 +0800

    键盘输入遮挡问题

--- AvaloniaApp.Android/MainActivity.cs
+++ AvaloniaApp.Android/MainActivity.cs
@@ -1,8 +1,9 @@
-﻿using Android.App;
+﻿using System.Threading;
+using Android.App;
 using Android.Content.PM;
 using Avalonia;
 using Avalonia.Android;
-
+using Android.Views;
 namespace AvaloniaApp.Android;
 
 [Activity(
@@ -10,7 +11,8 @@ namespace AvaloniaApp.Android;
     Theme = "@style/MyTheme.NoActionBar",
     Icon = "@drawable/icon",
     MainLauncher = true,
-    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode,
+    WindowSoftInputMode = SoftInput.AdjustResize)]
 public class MainActivity : AvaloniaMainActivity<App>
 {
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
